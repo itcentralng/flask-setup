502a221a72ba01b7339a312d733937a25d4d29fb
Nasir Mustapha <teboy.com@gmail.com>
2020-10-31 15:20:38 +0100

    bumped version to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='flask_setup',  
-     version='0.2.1',
+     version='0.2.2',
      scripts=['flask_setup'] ,
      author="Nasir Mustapha",
      author_email="nasir@mrteey.com",
