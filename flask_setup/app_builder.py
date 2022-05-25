d227fafbf270dd407e74ea9cadbc8154632e5a5c
Nasir Mustapha <teboy.com@gmail.com>
2022-05-25 13:08:11 +0100

    replace env DATABASE_URI with DATABASE_URL

--- flask_setup/app_builder.py
+++ flask_setup/app_builder.py
@@ -1,8 +1,4 @@
-# from flask_setup.file_strings import configuration, standard_app, basic_app, standard_filters, basic_blueprint_app, basic_blueprint_setup, standard_blueprint_app, standard_blueprint_setup, standard_methods, standard_model, standard_marshmallow, standard_filters
-
 from flask_setup.methods import install, get_project_name
-
-# from flask_setup.config import args
 
 import os
 
