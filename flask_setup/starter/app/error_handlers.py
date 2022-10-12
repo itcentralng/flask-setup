354784db737fcd9e19dfdceb8094243339081c74
Nasir Mustapha <teboy.com@gmail.com>
2022-10-12 12:07:34 +0100

    add more error handlers

--- flask_setup/starter/app/error_handlers.py
+++ flask_setup/starter/app/error_handlers.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from marshmallow import ValidationError
-from app import app
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError, OperationalError, DatabaseError, DataError
+from app import app, db
 
 
 @app.errorhandler(ValueError)
@@ -17,5 +18,59 @@ def handle_validation_error(error):
         'status': 'error',
         'fields': error.messages
 
+    }
+    return response, 400
+
+@app.errorhandler(SQLAlchemyError)
+def handle_sqlalchemy_error(error):
+    db.session.rollback()
+    response = {
+        'status': 'error',
+        'message': str(error)
+    }
+    return response, 400
+
+@app.errorhandler(IntegrityError)
+def handle_integrity_error(error):
+    db.session.rollback()
+    response = {
+        'status': 'error',
+        'message': str(error)
+    }
+    return response, 400
+
+@app.errorhandler(OperationalError)
+def handle_operational_error(error):
+    db.session.rollback()
+    response = {
+        'status': 'error',
+        'message': str(error)
+    }
+    return response, 400
+
+@app.errorhandler(DatabaseError)
+def handle_database_error(error):
+    db.session.rollback()
+    response = {
+        'status': 'error',
+        'message': str(error)
+    }
+    return response, 400
+
+@app.errorhandler(DataError)
+def handle_data_error(error):
+    db.session.rollback()
+    response = {
+        'status': 'error',
+        'message': str(error)
+    }
+    return response, 400
+
+@app.errorhandler(Exception)
+def handle_exception(error):
+    db.session.rollback()
+    response = {
+        'status': 'error',
+        'message': str(error)
     }
     return response, 400
