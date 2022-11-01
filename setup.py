d17226ba198202f410342c13d78f75493f914b5f
Nasir Mustapha <teboy.com@gmail.com>
2022-11-01 13:42:16 +0100

    bump to 0.5.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     setup(
      name='flask_setup',  
-     version='0.5.2',
+     version='0.5.3',
      scripts=['fs'] ,
      author="Nasir Mustapha",
      author_email="nasir@mrteey.com",
