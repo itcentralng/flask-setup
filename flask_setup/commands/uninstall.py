6cdaea87d2d292eaaea80761a0284513c810f76b
Nasir Mustapha <teboy.com@gmail.com>
2024-08-07 23:37:45 +0100

    Update .fs dependency uninstall command

--- flask_setup/commands/uninstall.py
+++ flask_setup/commands/uninstall.py
@@ -1,15 +1,21 @@
 import os
 import typer
 
-from flask_setup.methods import do_add_log, do_freeze
+from flask_setup.methods import do_add_log, do_freeze, read_logs, write_logs
+
+PIP = 'venv/bin/pip' if os.name == 'posix' else 'venv\Scripts\pip'
 
 def run_uninstall_command(package):
     if package == 'all':
-        os.system('pip uninstall -r requirements.txt')
-        log = 'All packages uninstalled successfully'
+        os.system(f'{PIP} freeze --exclude-editable | xargs pip uninstall -y')
+        logs = read_logs()
+        logs = logs['packages'] = []
+        write_logs(logs)
     else:
-        log = f'Package: {package} uninstalled successfully'
-        os.system(f'pip uninstall {package}')
-    do_freeze()
-    typer.echo(log)
-    do_add_log(log)
+        os.system(f'{PIP} uninstall {package}')
+        logs = read_logs()
+        for index, item in enumerate(logs['packages']):
+            if item['name'].lower() == package.lower():
+                logs['packages'].pop(index)
+                write_logs(logs)
+                break
