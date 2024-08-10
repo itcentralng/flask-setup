7b5da1b1db0b5d524357b55ee91cf437a12a3d2f
Nasir Mustapha <teboy.com@gmail.com>
2024-08-10 20:53:10 +0100

    add progressbar to build command

--- flask_setup/commands/build.py
+++ flask_setup/commands/build.py
@@ -5,37 +5,41 @@ import typer
 from flask_setup.commands.install import install_defaults, manage_dependencies
 from flask_setup.methods import write_log_file
 from shutil import copytree
+from rich.progress import track
 
 def run_build_command(project, name, email, path):
     """
     Build a new project from scratch
     """
     if os.path.exists(project):
-            typer.echo("Project already exists")
-            return
-        
-    # Create project folder
-    os.mkdir(project)
-    # move to project folder
-    os.chdir(project)
-    # do_add_log("Project folder created")
-
-    # copy relevant app files to project directory
-    copytree(f'{path}/starter', '.', dirs_exist_ok=True)
+        typer.echo("Project already exists")
+        return
     
-    # rename __project__ in app/main.py
-    with open("main.py", "r") as main_app:
-        content = main_app.read()
-        content = content.replace("__project__", project)
-        with open("main.py", "w") as main_app:
-            main_app.write(content)
-    # create virtual environment
-    os.system('python3 -m venv venv') if os.name == 'posix' else os.system('py -m venv venv')
+    for _ in track(range(1), description="Creating project folder..."):
+            
+        # Create project folder
+        os.mkdir(project)
+        # move to project folder
+        os.chdir(project)
+        
+        # copy relevant app files to project directory
+        copytree(f'{path}/starter', '.', dirs_exist_ok=True)
+        
+        # rename __project__ in app/main.py
+        with open("main.py", "r") as main_app:
+            content = main_app.read()
+            content = content.replace("__project__", project)
+            with open("main.py", "w") as main_app:
+                main_app.write(content)
+    
+        # create virtual environment
+        os.system('python3 -m venv venv') if os.name == 'posix' else os.system('py -m venv venv')
 
-    PIP = "venv/bin/pip" if os.name == 'posix' else  "venv\\Scripts\\pip"
+    for _ in track(range(1), description="Upgrading pip..."):
+        PIP = "venv/bin/pip" if os.name == 'posix' else  "venv\\Scripts\\pip"
 
-    # upgrade pip
-    subprocess.run([PIP, 'install', '--upgrade', 'pip'], stdout=subprocess.PIPE, text=True)
+        # upgrade pip
+        subprocess.run([PIP, 'install', '--upgrade', 'pip'], stdout=subprocess.PIPE, text=True)
 
     write_log_file(project, name, email)
 
